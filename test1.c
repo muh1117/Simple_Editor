4642a4d6084e13bb5e946b74b5957a649976a47a
muh1117 <muh1117@naver.com>
2014-04-17 11:20:38 +0900

    test1.c
    	fixed.

--- test1.c
+++ test1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+
 	fputs("\033[6;10H", stdout);
-	fputs("\033[6;10HHello\n", stdout);
+	fputs("\033[6;10HHello\n", stdout);//cursor moving
 
 	return 0;
 }
